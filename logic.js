bb0c5c3a91a23005fd1f2cbbb879ed0a72da94ad
Daniel Rico <dericop@unal.edu.co>
2016-05-09 17:49:36 -0500

    Generación del árbol

--- logic.js
+++ logic.js
@@ -14,6 +14,9 @@ $(document).ready(function(){
                 Container: "canvas"
     });
     
+    pk_ada.prototype.currentNode = 0; // Nodo actual para graficar sus hijos
+
+    pk_ada.prototype.curId = 1; // este es el padre
 
     pk_ada.prototype.init = function(){
         /*
@@ -22,9 +25,16 @@ $(document).ready(function(){
         $('#modalLoadContent').openModal();
     };
 
-    pk_ada.prototype.drawNode = function(){
-        $("#canvas").append("<div class='window' id='chartWindow1'>n = 16</div");
-        var node = $("#chartWindow1")[0];
+    pk_ada.prototype.dismiss = function(){
+        //pk_ada.curId -= 1;
+    }
+
+    pk_ada.prototype.drawNode = function(padre){
+        pk_ada.currentNode += 1;
+
+        $("#canvas").append("<div class='window' id='chartWindow"+pk_ada.currentNode+"'></div");
+        var node = $("#chartWindow"+pk_ada.currentNode)[0];
+        
         pk_ada.instance.addEndpoint(node, {
                     uuid: node.getAttribute("id") + "-bottom",
                     anchor: "Bottom",
@@ -36,18 +46,30 @@ $(document).ready(function(){
             anchor: "Top",
             maxConnections: -1
         });
-    }
 
-    pk_ada.prototype.executeCode = function(){
-
-         if (editor) {
-
+        if (pk_ada.currentNode != 1) {
             // Algunos elementos comunes
             var arrowCommon = { foldback: 0.7, fillStyle: "blue", width: 14 },
             // Creación del tipo de flecha en la conexión
             overlays = [
                 [ "Arrow", { location: 0.8 }, arrowCommon ]
             ];
+
+            //console.log("chartWindow"+(pk_ada.currentNode - 1)+"-bottom");
+            pk_ada.instance.connect({uuids: ["chartWindow"+(padre)+"-bottom", "chartWindow"+pk_ada.currentNode+"-top" ], overlays: overlays});
+        }
+
+        console.log("Padre: "+padre+" CurrentNode:"+pk_ada.currentNode);
+
+        return pk_ada.currentNode;
+
+        
+
+    }
+
+    pk_ada.prototype.executeCode = function(){
+
+         if (editor) {
             
             // --------------------- Ejecución del código --------------------------------
             var code = editor.getValue();
@@ -115,22 +137,40 @@ $(document).ready(function(){
         var code = ""; // código del algoritmo seleccionado.
         switch(event.target.id){ 
             case "0": // Torres de hanoi
-                code = 'function hanoi(n, origen, auxiliar, destino){\n'+
-                            '\t pk_ada.drawNode();\n'+
+                code = 'function hanoi(n, origen, auxiliar, destino, padre){\n'+
+                            '\t padre = pk_ada.drawNode(padre);\n'+
                             '\t if(n==1)\n'+
                                 '\t\t console.log("mover disco de origen a destino");\n'+
                             '\t else{\n'+
-                                '\t\t hanoi(n-1, origen, destino, auxiliar);\n'+
+                                '\t\t hanoi(n-1, origen, destino, auxiliar, padre);\n'+
                                 '\t\t console.log("mover disco de origen a destino");\n'+
-                                '\t\t hanoi(n-1, auxiliar, origen, destino);\n'+
-                            '\t}\n'+
+                                '\t\t hanoi(n-1, auxiliar, origen, destino, padre);\n'+
+                            '\t }\n'+
                         '};\n'+
-                        'hanoi(4, "o", "d", "a");'
+                        'hanoi(3, "o", "d", "a");'
 
                 break;
-            case "1": // Reinas
+            case "1": // factorial
+                code = 'function factorial(n, padre){\n'+
+                        '\t padre = pk_ada.drawNode(padre);\n'+
+                        '\t if((n === 0) || (n === 1))\n'+
+                        '\t\t return 1;\n'+
+                        '\t else{\n'+
+                        '\t\t return n*factorial(n-1, padre);\n'+
+                        '\t }\n'+
+                        '}\n'+
+                        'factorial(5);'
                 break;
             case "2": // Fibonacci
+                code = 'function fibonacci(n, padre){\n'+
+                        '\t padre = pk_ada.drawNode(padre);\n'+
+                        '\t if((n === 0) || (n === 1))\n'+
+                        '\t\t return 1;\n'+
+                        '\t else{\n'+
+                        '\t\t return fibonacci(n-1, padre) + fibonacci(n-2, padre);\n'+
+                        '\t }\n'+
+                        '}\n'+
+                        'fibonacci(3);'
                 break;
             case "3": // Mergesort
                 break;
